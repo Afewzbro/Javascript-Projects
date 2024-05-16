50d0e8cf044515b5bc182601f50ab127a96785de
Afewzbro <testiafendi1992@gmail.com>
2024-05-16 19:58:01 -0400

    palindrome

--- Date Formatter/script.js
+++ Date Formatter/script.js
@@ -23,8 +23,8 @@ dateOptionsSelectElement.addEventListener("change", () => {
     case "mm-dd-yyyy-h-mm":
       currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
       break;
-    defualt:
-    currentDateParagraph.textContent = formattedDate;
+    default:
+        currentDateParagraph.textContent = formattedDate;
     
   }
 });
